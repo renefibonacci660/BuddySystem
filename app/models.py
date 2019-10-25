341c5b5b8b6743e981d1814388d06cb9d9c25aec
renefibonacci660 <660@holbertonschool.com>
2019-10-25 12:55:15 -0700

    Added method that creates a token for user login and verifies so the same token cannot be used by another user to log in

--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
 from . import db
 from . import login_manager
+from flask import current_app
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
 
 
 class Role(db.Model):
     __tablename__ = 'roles'
+    # __table_args__ = {'extend_existing': True} 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
@@ -17,11 +20,30 @@ class Role(db.Model):
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
+    # __table_args__ = {'extend_existing': True} 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(64, unique=True, index=True))
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default=False)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
 
     @property
     def password(self):
