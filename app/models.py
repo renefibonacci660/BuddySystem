16f52ee862c9479071544be8ee41c973033df6b3
renefibonacci660 <660@holbertonschool.com>
2019-10-29 14:11:47 -0700

    Added ability to change password and permission sets

--- app/models.py
+++ app/models.py
@@ -7,15 +7,66 @@ from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
 
 
+class Permission:
+    FOLLOW = 1
+    COMMENT = 2
+    WRITE = 4
+    MODERATE = 8
+    ADMIN = 16
+
+
 class Role(db.Model):
     __tablename__ = 'roles'
-    # __table_args__ = {'extend_existing': True} 
+    # __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permissions(self, perm):
+        self.permissions = 0
+
+    def has_permissions(self, perm):
+        return self.permissions & perm == perm
+
+
+@staticmethod
+def insert_roles)():
+    roles = {
+        'User': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE],
+        'Moderator': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE, Permission.MODERATE],
+        'Administrator': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE, Permission.MODERATE, Permission.ADMIN],
+    }
+    default_role = 'User'
+    for i in roles:
+        role = Role.query.filter_by(name=i).first()
+        if role is None:
+            role = Role(name=i)
+        role.reset_permissions()
+        for perm in roles[i]:
+            role.add_permission(perm)
+        role.default = (role.name == default_role)
+        db.session.add(role)
+    db.session.commit()
+
 
 
 class User(UserMixin, db.Model):
@@ -44,6 +95,19 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.comfig['FLASKY_ADMIN']:
+                self.role = Role. query.filter_by(name='Administrator').first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
 
     @property
     def password(self):
@@ -59,7 +123,17 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+login_manager.anonymous_user = AnonymousUser
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
