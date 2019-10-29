73de440143b520e795b0f19ae5adbdde3e3c4b7a
renefibonacci660 <660@holbertonschool.com>
2019-10-29 12:57:19 -0700

    Custom decorators that check the user permissions

---
+++ app/decorators.py
@@ -0,0 +1,20 @@
+from flask import abort
+from flask_login import current_user
+from functools import wraps
+
+from .models import Permission
+
+
+def permission_required(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.can(permission):
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+
+def admin_required(f):
+    return permission_required(Permission.ADMIN)(f)
