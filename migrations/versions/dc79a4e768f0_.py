f70893c8137199599d752d18ff3d1d2264b2c97d
renefibonacci660 <660@holbertonschool.com>
2019-10-29 12:11:52 -0700

    Adding migrations files used for creating columns in db thru flask

---
+++ migrations/versions/dc79a4e768f0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: dc79a4e768f0
+Revises: 
+Create Date: 2019-10-24 20:08:47.448254
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dc79a4e768f0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
