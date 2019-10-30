49f7e8d2abc42f3f983f0783db8705538f567265
renefibonacci660 <660@holbertonschool.com>
2019-10-30 15:20:19 -0700

    Added Edit Profile class for each individual user

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, BooleanField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, Regexp
+from wtforms import ValidationError
+from ..models import Role, User
 
 
 class NameForm(FlaskForm):
     name = StringField('What is your name gorgeous?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Full Name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About Me')
     submit = SubmitField('Submit')
