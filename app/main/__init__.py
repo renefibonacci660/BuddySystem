cb9fcc1b517e9c6b19ad1a663fe3e514688b7ff3
renefibonacci660 <660@holbertonschool.com>
2019-10-30 15:19:25 -0700

    Reverted changes

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,9 +4,3 @@ main = Blueprint('main', __name__)
 
 from . import errors
 from . import views
-from ..models import Permission
-
-
-@main.app_context_processor
-def inject_permissions():
-    return dict(Permission=Permission)
