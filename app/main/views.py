5653b841887c52b4796b77911ca81baab9c8d664
renefibonacci660 <660@holbertonschool.com>
2019-10-30 15:23:05 -0700

    Added route for editing current user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,16 @@
+from flask import flash
 from flask import render_template
 from flask import session
 from flask import redirect
 from flask import url_for
 from flask import current_app
+from flask_login import login_required
+from flask_login import current_user
 from .. import db
 from ..models import User
 from ..email import send_email
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -30,3 +33,25 @@ def index():
     return render_template('index.html',
                            form=form, name=session.get('name'),
                            known=session.get('known', False))
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
